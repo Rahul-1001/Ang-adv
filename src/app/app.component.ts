1fd684ad9de988665fe55ff3f7cc267e2a810dee
Rahul-1001 <projectmaterial09@gmail.com>
2025-03-15 13:13:02 +0530

    input field get and set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignupComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular';
+
+//  handleclickevent(){
+//   console.log("function called")
+//   //important point  
+//   this.otherFunction()
+//  }
+
+//  otherFunction()
+//  {
+//   console.log("other function call")
+//  }
+
+
+//  count:number=0;
+//  handleIncrement()
+//  {
+//   this.count=this.count+1;
+//  }
+//  handleDecrement()
+//  {
+//   this.count=this.count-1;
+//  }
+ 
+//  handleReset()
+//  {
+//   this.count=0
+//  }
+
+
+
+
+// handleEvent(event:any)
+// {
+//   console.log("function called ",event.type)
+//   console.log("Value  ",(event.target as HTMLInputElement).value)
+// }
+
+
+
+
+
+
+name=""
+displayName=""
+email=""
+getName(event:Event)
+{
+  // const val=(event.target as HTMLInputElement).value
+  this.name=(event.target as HTMLInputElement).value
+  // console.log(val)
+  // this.name=val
+}
+showName()
+{
+  this.displayName=this.name 
+}
+
+setName()
+{
+  this.name="rah"
+}
+
+
 }
